4bc42a3afd4d3a484fb8dc427db14b001191679d
dpetrov <dpetrov@cyl.me>
2016-11-02 19:48:12 +0300

    Add db module which connect to sqlite db create table and close it and search nonexists :)

---
+++ src/db.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+from typing import List
+
+
+def initialize(connection: str) -> sqlite3.Connection:
+    """
+    Connect to database and create table if is not exists
+    """
+    conn = sqlite3.connect(connection)
+
+    create_table(conn)
+    return conn
+
+
+def create_table(conn: sqlite3.Connection):
+    """
+    Create table in DB with column for timestamp and column for name
+    """
+    with conn:
+        conn.execute("CREATE TABLE documents (timestamp int, name text)")
+
+
+def finalize(conn: sqlite3.Connection):
+    """
+    Close connection to DB
+    """
+    conn.close()
+
+
+def nonexists(conn: sqlite3.Connection, candidates: List[str]) -> List[str]:
+    """
+    Find candidates from candidates list which is not exists yet in DB.
+    """
+    statement = "SELECT name FROM documents WHERE name IN ({})".format(
+        ", ".join(['?'] * len(candidates)))
+    with conn:
+        exists_candidates = conn.execute(statement, candidates).fetchall()
+    return list(set(candidates).difference(set(map(lambda x: x[0],
+                                                   exists_candidates))))
